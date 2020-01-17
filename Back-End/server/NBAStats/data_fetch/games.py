9e882ef63040258ad9086e32123985f8062b3670
Joshua Domenico Lembong <lembongjoshua@gmail.com>
2020-01-18 02:15:06 +0700

    Added outline for games handler

---
+++ Back-End/server/NBAStats/data_fetch/games.py
@@ -0,0 +1,13 @@
+#This file will be responsible for handling the requests to receive the recent games played and any games played given two teams. 
+import json 
+import request 
+import os 
+
+def getRecentGames(): 
+    pass
+
+def getGameHistory(teamA, teamB):
+    pass
+
+def searchTeamGames(team): 
+    pass
